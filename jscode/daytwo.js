95d457781f56c044f6c6860b37c39fef4c9b0c3c
AntonDolg <modbus7@gmail.com>
2023-12-24 23:44:42 +0300

    daytwo

--- jscode/daytwo.js
+++ jscode/daytwo.js
@@ -1,17 +1,21 @@
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat:false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies:{},
+//     actors:{},
+//     genres:[],
+//     privat:false
+// };
 
-const lastFilm = prompt("Один из последних просмотренных фильмов?");
-const markFilm = +prompt("На сколько оцените его?");
+// const last1Film = prompt("Один из последних просмотренных фильмов?"),
+// mark1Film = +prompt("На сколько оцените его?"),
+// last2Film = prompt("Один из последних просмотренных фильмов?"),
+// mark2Film = +prompt("На сколько оцените его?");
 
-const movies = {
-    last:lastFilm,
-    mark:markFilm
-};
+// personalMovieDB.movies[last1Film]=mark1Film;
+// personalMovieDB.movies[last2Film]=mark2Film;
+
+// console.log(personalMovieDB);
+
+
