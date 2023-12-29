6c4352357f0352ca3a502fdb82e5092c175e5a67
AntonDolg <modbus7@gmail.com>
2023-12-29 13:52:52 +0300

    NG

---
+++ jscode/daythree.js
@@ -0,0 +1,12 @@
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+else{
+    console.log((hamburger && cola || fries === 3 && nuggets))
+}
--- jscode/daytwo.js
+++ jscode/daytwo.js
@@ -19,3 +19,56 @@
 // console.log(personalMovieDB);
 
 
+const personalMovieDB = {
+    user:[],
+    numberOfFilms:[],
+    lastFilm:[],
+    markFilm:[],
+    test:{
+        fun:function(){
+        console.log(`error:${i}}`)
+    }
+    }
+};
+
+// for(let i=1;i<10;i++){
+//     personalMovieDB.numberOfFilms[i] = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//     personalMovieDB.lastFilm[i] = prompt("Один из последних просмотренных фильмов?","");
+//     personalMovieDB.markFilm[i] = +prompt("На сколько оцените его?","");
+//     let stopQ = confirm("Остановиться?");
+//     if(stopQ==true || i==3){
+//         break;
+//     }
+// }
+
+//const numberOfUser = +prompt("Номер пользователя?", "");
+
+//alert(personalMovieDB.lastFilm[numberOfUser]);
+const i = 0;
+Repit:while(true){
+    personalMovieDB.numberOfFilms[i]=+prompt("Сколько фильмов?", "");
+    personalMovieDB.lastFilm[i]=prompt("Какой фильм вам понравился?", "");
+    if(personalMovieDB.numberOfFilms[i].length>2 || personalMovieDB.lastFilm[i].length>8){
+        continue Repit;
+    }
+    if(personalMovieDB.numberOfFilms[i].length===0 || personalMovieDB.lastFilm[i].length===0){
+        continue Repit;
+    }
+    if(personalMovieDB.numberOfFilms[i]<10){
+        alert("Мало фильмов");
+    }
+    else if(personalMovieDB.numberOfFilms[i]>=10 && personalMovieDB.numberOfFilms[i]<30){
+        alert("Много фильмов");
+    }
+    else{
+        alert("Вы киноман");
+    }
+    let stopRun = confirm("Остановка?");
+    if(stopRun===true){
+        break;
+    }
+}
+
+console.log(Object.keys(personalMovieDB).length);
+const{fun}=personalMovieDB.test;
+alert(fun);
